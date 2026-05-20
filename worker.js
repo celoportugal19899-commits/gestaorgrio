e71c2e64fa3595337bcd14683c7be1b8e17f41aa
celoportugal19899-commits <celoportugal19899@gmail.com>
2026-05-20 16:18:46 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    const asset = await env.ASSETS.fetch(request);
-    return new Response(asset.body, {
-      status: asset.status,
-      headers: {
-        ...Object.fromEntries(asset.headers),
-        'Content-Type': 'text/html; charset=utf-8'
-      }
-    });
+    return env.ASSETS.fetch(request);
   }
-}
+};
